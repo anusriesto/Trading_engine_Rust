4d4aac82625d3de139b8aea219908819483dff27
Anuj Srivastava <anujwrap@live.com>
2023-07-11 23:41:11 +0530

    after 2nd lesson

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ fn main() {
     orderbook.add_order(2.6, sell_order);
     let mut engine=MatchingEngine::new();
     let pair=TradingPair::new("BTC".to_string(),"Sol".to_string());
-    engine.add_new_market(pair);
+    engine.add_new_market(pair.clone());
+
+    let buy_order=Order::new(BidOrAsk::Bid,6.5);
+    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
 
 
 
--- src/matching_engine/engine.rs
+++ src/matching_engine/engine.rs
@@ -1,4 +1,4 @@
-use super::orderbook::OrderBook;
+use super::orderbook::{OrderBook,Order};
 use std::collections::HashMap;
 
 //quote is what we sell in the return of base
@@ -13,6 +13,10 @@ pub struct TradingPair{
 impl TradingPair{
     pub fn new(base:String,quote:String)->TradingPair{
         TradingPair{base,quote}
+    }
+    pub fn to_string(self)->String{
+        format!("{}_{}",self.base,self.quote)
+
     }
 }
 
@@ -31,6 +35,23 @@ impl MatchingEngine{
 
     pub fn add_new_market(&mut self,pair:TradingPair){
         self.orderbooks.insert(pair.clone(),OrderBook::new());
-        println!("Creating new orderbook for market {:?}",pair);
+        println!("Creating new orderbook for market {:?}",pair.to_string());
+    }
+
+    pub fn place_limit_order(&mut self,
+    pair:TradingPair,price:f64,order:Order)->Result<(),String>{
+        match self.orderbooks.get_mut(&pair){
+            Some(orderbook)=>{
+                orderbook.add_order(price, order);
+
+                println!("the limit order is set at the price level{:?}",price);
+                Ok(())
+            }
+            None=>{
+                Err(format!(
+                    "The order book for given ({}) pair doesn't exist",pair.to_string()
+                ))
+            }
+        }
     }
 }
